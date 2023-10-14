03168fb733d4fdfeddc8be9d80a29ab05b7223d3
Paul David Tan <cryxuz@gmail.com>
2023-10-15 09:19:07 +1300

    get route working, booking list component workign no styles

--- client/apis/booking.ts
+++ client/apis/booking.ts
@@ -2,6 +2,10 @@ import request from 'superagent'
 
 const rootUrl = '/api/v1'
 
+export async function getBookingList() {
+  const dbList = await request.get(`${rootUrl}/bookings`)
+  return dbList.body
+}
 export async function addBookings(booking) {
   console.log('api hit')
   const addedBooking = await request.post(`${rootUrl}/bookings`).send(booking)
--- client/components/BookingList.tsx
+++ client/components/BookingList.tsx
@@ -1,4 +1,32 @@
+import { useQuery } from '@tanstack/react-query'
+import { getBookingList } from '../apis/booking'
+
 function BookingList() {
-  return <div>hello</div>
+  const {
+    data: bookings,
+    isLoading,
+    error,
+  } = useQuery(['bookings'], getBookingList)
+
+  if (error) {
+    return <p>Something went wrong.</p>
+  }
+  if (isLoading || !bookings) {
+    return <p>Loading... Please wait.</p>
+  }
+  return (
+    <div>
+      {bookings.map((el) => {
+        return (
+          <ul key={el.id}>
+            <li>Date: {el.date}</li>
+            <li>Name: {el.name}</li>
+            <li>Seats: {el.seats}</li>
+            <li>Time: {el.time}</li>
+          </ul>
+        )
+      })}
+    </div>
+  )
 }
 export default BookingList
