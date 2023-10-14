7b6266703c00e3c477cd41e6dcae46f1965d896f
Paul David Tan <cryxuz@gmail.com>
2023-10-15 09:06:00 +1300

    server side routes for get done and working

--- server/routes/bookings.ts
+++ server/routes/bookings.ts
@@ -1,8 +1,18 @@
 import { Router } from 'express'
 import { addBookings } from '../db/db.ts'
+import * as db from '../db/db.ts'
 
 const router = Router()
 
+router.get('/', (req, res) => {
+  db.getBookingList()
+    .then((booking) => {
+      res.json(booking)
+    })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
+})
 router.post('/', async (req, res) => {
   const booking = req.body
   const addedBooking = await addBookings(booking)
