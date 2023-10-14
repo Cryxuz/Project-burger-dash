52c631d5cf31120884acbce64040771a47eb1d45
Paul David Tan <cryxuz@gmail.com>
2023-10-15 09:02:30 +1300

    db.ts file for get booking list done

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,5 +1,10 @@
 import connection from './connection.ts'
 const db = connection
+
+export async function getBookingList() {
+  return db('bookings').select('*')
+}
+
 export async function addBookings(booking) {
   return db('bookings')
     .insert({ ...booking })
