eee9a155e5d4e0f404f1ce491e7b0f45bc09c90a
Paul David Tan <cryxuz@gmail.com>
2023-10-15 14:27:37 +1300

    test fail

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,19 +1,12 @@
-import Booking from './Booking'
-import CarouselPage from './CarouselPage'
 import Nav from './Nav'
 import { Outlet } from 'react-router-dom'
-import Story from './Story'
-import Contact from './Contact'
 
 function App() {
   return (
     <>
       <Nav />
-      <CarouselPage />
-      <Booking />
-      <Story />
-      <Contact />
-      {/* <Outlet /> */}
+
+      <Outlet />
     </>
   )
 }
