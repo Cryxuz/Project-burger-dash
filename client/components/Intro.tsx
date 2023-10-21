1fba29a99e87ceb731a568a3ab521248eb888041
Paul David Tan <cryxuz@gmail.com>
2023-10-21 19:46:32 +1300

    deleted eat visit drinks links

--- client/components/Intro.tsx
+++ client/components/Intro.tsx
@@ -1,39 +1,13 @@
 import { useState, useEffect } from 'react'
-import home from '/images/home.webp'
-import eatImage from '/images/carousel-img-2.webp'
-import drinkImage from '/images/drinks.webp'
-import visitImage from '/images/visit.webp'
+import main from '/images/visit.jpg'
 import Footer from './Footer'
-import { Link } from 'react-router-dom'
 
 function Intro() {
-  const [bgImage, setBgImage] = useState(home)
-  const [isVisible, setIsVisible] = useState(false)
-
-  const handleEatHover = () => {
-    setBgImage(eatImage)
-  }
-
-  const handleDrinkHover = () => {
-    setBgImage(drinkImage)
-  }
-
-  const handleVisitHover = () => {
-    setBgImage(visitImage)
-  }
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsVisible(true)
-    }, 500)
-    return () => clearTimeout(timer)
-  }, [])
+  const [bgImage, setBgImage] = useState(main)
 
   return (
     <div
-      className={`w-screen h-screen overflow-hidden bg-cover bg-no-repeat bg-center p-[15%] items-center transition-opacity duration-700 ${
-        isVisible ? 'opacity-100' : 'opacity-30'
-      }`}
+      className={`w-screen h-screen overflow-hidden bg-cover bg-no-repeat bg-center p-[15%] items-center transition-opacity duration-700 `}
       style={{ backgroundImage: `url(${bgImage})` }}
     >
       <div className="pt-[50%] md:mb-[5%] md:pt-[0%] text-center text-white">
@@ -43,28 +17,7 @@ function Intro() {
         </p>
       </div>
       <div></div>
-      <div className="pt-[20%] md:pt-0 md:flex text-center justify-center text-white">
-        <div
-          className="text-3xl md:text-6xl hover:underline py-[2%] px-[5%]"
-          onMouseEnter={handleEatHover}
-        >
-          <Link to="/menu">Eat</Link>
-        </div>
 
-        <div
-          className="text-3xl md:text-6xl hover:underline py-[2%] px-[5%]"
-          onMouseEnter={handleDrinkHover}
-        >
-          <Link to="/menu">Drink</Link>
-        </div>
-
-        <div
-          className="text-3xl md:text-6xl hover:underline py-[2%] px-[5%]"
-          onMouseEnter={handleVisitHover}
-        >
-          <Link to="/contact">Visit</Link>
-        </div>
-      </div>
       <div className="flex items-end justify-end fixed bottom-0 right-0 left-0 pb-2 px-10">
         <Footer />
       </div>
